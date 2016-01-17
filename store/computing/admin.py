1919f5b7de349ee0c4fa8aade1e019debcf7a823
João Carlos <jcbalmeida@gmail.com>
2016-01-17 12:26:53 -0200

    Adicionando os models

--- store/computing/admin.py
+++ store/computing/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Provider,
+    OsFamily,
+    OperatingSystem,
+    Instance,
+    ServerPlan)
+
+
+admin.site.register(Provider)
+admin.site.register(OsFamily)
+admin.site.register(OperatingSystem)
+admin.site.register(Instance)
+admin.site.register(ServerPlan)
--- store/computing/models.py
+++ store/computing/models.py
@@ -2,4 +2,44 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Provider(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class OsFamily(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class OperatingSystem(models.Model):
+    name = models.CharField(max_length=50)
+    version = models.CharField(max_length=50)
+    family = models.ForeignKey(OsFamily, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.version)
+
+
+class Instance(models.Model):
+    name = models.CharField(max_length=50)
+    v_cpu = models.IntegerField()
+    memory = models.FloatField()
+    disk_space = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class ServerPlan(models.Model):
+    name = models.CharField(max_length=50)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
