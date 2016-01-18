26041b6950c9276ff6c4dc2f65e868706983d526
João Carlos <jcbalmeida@gmail.com>
2016-01-18 02:54:11 -0200

    ajuste na configuração dos staticfiles

--- store/settings.py
+++ store/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/joao/workspace-joao/onecloud-store/static'
+STATIC_ROOT = os.path.join('static')
 TATICFILES_DIRS = (os.path.join('static'), )
 
 REST_FRAMEWORK = {
